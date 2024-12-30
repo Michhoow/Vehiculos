b7c15bcb9feacd5e217d212f578f2730d010aa3e
Michhoow <michaelmartinez1604@gmail.com>
2024-12-30 00:04:45 -0400

    Operaciones CRUD completadas

--- CodeFile1.cs
+++ CodeFile1.cs
@@ -4,7 +4,7 @@ public class DataBase
 {
     string db = @"Server=MICHAEL;Database=Vehiculosdb;Trusted_Connection=True;TrustServerCertificate=True;";
     string query = "Select * from Vehiculos";
-  
+
     public void Test()
     {
         using SqlConnection connection = new SqlConnection(db);
@@ -19,15 +19,15 @@ public class DataBase
             using SqlDataReader reader = command.ExecuteReader();
 
             Console.WriteLine("Datos de la tabla Vehículos:");
-            Console.WriteLine(new string('-', 80));
+            Console.WriteLine(new string('-', 154));
             for (int i = 0; i < reader.FieldCount; i++)
             {
                 Console.Write($"| {reader.GetName(i),-15}");
             }
             Console.WriteLine("|");
-            Console.WriteLine(new string('-', 80));
+            Console.WriteLine(new string('-', 154));
 
-            
+
             while (reader.Read())
             {
                 for (int i = 0; i < reader.FieldCount; i++)
@@ -36,7 +36,7 @@ public class DataBase
                 }
                 Console.WriteLine("|");
             }
-            Console.WriteLine(new string('-', 80));
+            Console.WriteLine(new string('-', 154));
 
         }
         catch (Exception ex)
@@ -50,12 +50,12 @@ public class DataBase
     public void agregar()
     {
         using SqlConnection connection = new SqlConnection(db);
-        
+
         try
         {
             connection.Open();
             Console.WriteLine("Conexion exitosa.");
-     
+
             Console.WriteLine($"Ingrese la marca del vehiculo: ");
             string Marca = Console.ReadLine();
             Console.WriteLine("Ingrese el modelo del vehiculo: ");
@@ -64,10 +64,19 @@ public class DataBase
             int fecha = int.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese el color del vehiculo: ");
             string Color = Console.ReadLine();
+            Color.ToLower();
+            while (Color != "azul" || Color != "rojo" || Color != "amarillo" || Color != "verde" || Color != "negro" || Color != "blanco")
+            {
+                Console.WriteLine("Color invalido, ingrese otra vez.");
+            }
             Console.WriteLine("Ingrese el Tipo de vehiculo(sedan,suv,cambion): ");
             string Tipo = Console.ReadLine();
             Console.WriteLine("Ingrese la placa(6 digitos): ");
             string placa = Console.ReadLine();
+            while (placa.Length < 6 || placa.Length > 6)
+            {
+                Console.WriteLine("La placa tiene que ser de 6 digitos.");
+            }
             Console.WriteLine("Ingrese los KM: ");
             int km = int.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese el precio: ");
@@ -92,19 +101,19 @@ public class DataBase
 
     public void borrar()
     {
-        using SqlConnection connection = new SqlConnection( db );
+        using SqlConnection connection = new SqlConnection(db);
         try
         {
             connection.Open();
             Console.WriteLine("Ingrese el ID del vehiculo a eliminar: ");
             string id = Console.ReadLine();
             string query3 = $"Delete from Vehiculos where id = {id}";
-            using SqlCommand sqlCommand = new SqlCommand( query3, connection);
-            
+            using SqlCommand sqlCommand = new SqlCommand(query3, connection);
+
             int rowsaffected = sqlCommand.ExecuteNonQuery();
 
 
-            if (rowsaffected >0)
+            if (rowsaffected > 0)
             {
                 Console.WriteLine("Vehiculo eliminado correctamente, pulse una tecla para continuar.");
                 Console.ReadKey();
@@ -115,6 +124,138 @@ public class DataBase
                 Console.WriteLine("No hay vehiculos con ese ID, pulse una tecla para continuar.");
                 Console.ReadKey();
             }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+    public void update()
+    {
+        using SqlConnection sqlConnection = new SqlConnection(db);
+        sqlConnection.Open();
+        try
+        {
+            DataBase dbData = new DataBase();
+            dbData.Test();
+            Console.WriteLine("Pulse una tecla para continuar...");
+            Console.ReadKey();
+
+            // Pedir ID o placa
+            Console.WriteLine("Ingrese el ID o placa del vehículo a actualizar: ");
+            var query4 = Console.ReadLine();
+            int query5;
+
+            if (!int.TryParse(query4, out query5))
+            {
+                Console.WriteLine("Ingrese el nombre de la columna a cambiar: ");
+                string columna = Console.ReadLine();
+                Console.WriteLine("Ingrese el nuevo valor: ");
+                var valor = Console.ReadLine();
+                int valor2;
+                if (int.TryParse(valor, out valor2))
+                {
+
+                    string consulta = $@"UPDATE Vehiculos SET {columna} = '{valor2}' WHERE placa = '{query4}'";
+                    using SqlCommand sqlCommand = new SqlCommand(consulta, sqlConnection);
+
+
+                    int rowsaffected = sqlCommand.ExecuteNonQuery();
+
+                    if (rowsaffected > 0)
+                    {
+                        Console.WriteLine(@$"Vehiculo actualizado con exito {rowsaffected}s columnas actualizadas.");
+                        Console.ReadKey();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No hay vehiculos con esa placa, pulse una tecla para continuar.");
+                        Console.ReadKey();
+                    }
+
+                }
+                else
+                {
+                    string consulta2 = $@"UPDATE Vehiculos SET {columna} = {valor} WHERE placa = '{query4}'";
+                    using SqlCommand sqlCommand = new SqlCommand(consulta2, sqlConnection);
+
+
+                    int rowsaffected = sqlCommand.ExecuteNonQuery();
+
+                    if (rowsaffected > 0)
+                    {
+                        Console.WriteLine(@$"Vehiculo actualizado con exito {rowsaffected}s columnas actualizadas.");
+                        Console.ReadKey();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("No hay vehiculos con esa placa, pulse una tecla para continuar.");
+                        Console.ReadKey();
+                    }
+
+                }
+            }
+
+
+
+
+            else
+            {
+                
+                    Console.WriteLine("Ingrese el nombre de la columna a cambiar: ");
+                    string columna = Console.ReadLine();
+                    Console.WriteLine("Ingrese el nuevo valor: ");
+                    var valor = Console.ReadLine();
+                    int valor2;
+                    if (int.TryParse(valor, out valor2))
+                    {
+
+                        string consulta = $@"UPDATE Vehiculos SET {columna} = '{valor2}' WHERE ID = {query4}";
+                        using SqlCommand sqlCommand = new SqlCommand(consulta, sqlConnection);
+
+
+                        int rowsaffected = sqlCommand.ExecuteNonQuery();
+
+                        if (rowsaffected > 0)
+                        {
+                            Console.WriteLine(@$"Vehiculo actualizado con exito {rowsaffected}s columnas actualizadas.");
+                            Console.ReadKey();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("No hay vehiculos con esa placa, pulse una tecla para continuar.");
+                            Console.ReadKey();
+                        }
+
+                    }
+                    else
+                    {
+                        string consulta2 = $@"UPDATE Vehiculos SET {columna} = {valor} WHERE placa = '{query4}'";
+                        using SqlCommand sqlCommand = new SqlCommand(consulta2, sqlConnection);
+
+
+                        int rowsaffected = sqlCommand.ExecuteNonQuery();
+
+                        if (rowsaffected > 0)
+                        {
+                            Console.WriteLine(@$"Vehiculo actualizado con exito {rowsaffected}s columnas actualizadas.");
+                            Console.ReadKey();
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("No hay vehiculos con esa placa, pulse una tecla para continuar.");
+                            Console.ReadKey();
+                        }
+
+
+                    }
+                }
+            
         }
 
         catch (Exception ex)
--- Program.cs
+++ Program.cs
@@ -1,8 +1,71 @@
 ﻿DataBase database = new DataBase();
 
+int index = 0;
+ConsoleKey key;
+string[] opciones = { "Ver vehiculos","Agregar","Borrar por ID", "Actualizar propiedades"};
+
+
+
 while (true)
 {
-    database.Test();
-    database.agregar();
-    database.borrar();
+    Console.Clear();
+    for (int i = 0; i < opciones.Length; i++)
+    {
+        if (index == i)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($">>{opciones[i]}<<");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.WriteLine(opciones[i]);
+        }
+    }
+
+    key = Console.ReadKey(true).Key;
+
+
+    if (key == ConsoleKey.W || key == ConsoleKey.UpArrow)
+    {
+        index--;
+        if (index < 0)
+        {
+            index = opciones.Length - 1;
+        }
+    }
+    else if (key == ConsoleKey.S || key == ConsoleKey.DownArrow)
+    {
+        index++;
+        if (index >= opciones.Length)
+        {
+            index = 0;
+        }
+    }
+
+    if (key == ConsoleKey.Enter && index == 0)
+    {
+        database.Test();
+        Console.WriteLine("Pulse una tecla para continuar...");
+        Console.ReadKey();
+    }
+    else if (key == ConsoleKey.Enter && index == 1)
+    {
+        database.agregar();
+        Console.WriteLine("Pulse una tecla para continuar...");
+        Console.ReadKey();
+    }
+    else if (key == ConsoleKey.Enter && index == 2)
+    {
+        database.borrar();
+        Console.WriteLine("Pulse una tecla para continuar...");
+        Console.ReadKey();
+    }
+    else if (key == ConsoleKey.Enter && index == 3)
+    {
+        database.update();
+        Console.WriteLine("Pulse una tecla para continuar");
+        Console.ReadKey();
+    }
+
 }
